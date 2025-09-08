ddfc3aa8e499adf5b45c6835217bd65a0b4145b8
mingkyeongg <ming0820@naver.com>
2025-09-08 12:08:55 +0900

    chore: chakra-ui 설정

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import { colors } from "@/utils/colors";
 import styled from "@emotion/styled";
 
 interface CardProps {
   children: React.ReactNode;
+  style?: React.CSSProperties;
 }
 
-export const Card = ({ children }: CardProps) => {
-  return <CardContainer>{children}</CardContainer>;
+export const Card = ({ children, style }: CardProps) => {
+  return <CardContainer style={style}>{children}</CardContainer>;
 }
 
 const CardContainer = styled.div`
