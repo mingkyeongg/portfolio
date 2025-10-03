1b54bc695a794bedecd8ddb3ba7173a3cf3a6996
mingkyeongg <ming0820@naver.com>
2025-10-04 03:36:37 +0900

    feat: 사이트 제목 변경

--- src/features/Profile.tsx
+++ src/features/Profile.tsx
@@ -1,11 +1,10 @@
 import { Card } from "@/components/Card/Card";
-import { HStack, Image, VStack } from "@chakra-ui/react";
+import { HStack, VStack } from "@chakra-ui/react";
 
 export const Profile = () => {
   return (
     <Card>
       <HStack w="100%" h="100%" alignItems="space-between">
-        <Image src="/images/profile.jpg" alt="profile" width={100} height={100} borderRadius="full" />
         <VStack w="100%" h="100%">Profile</VStack>
       </HStack>
     </Card>
