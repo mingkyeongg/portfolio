f7f5bb529a12f9617f12b6b39f8bbba4ef65e765
mingkyeongg <ming0820@naver.com>
2025-11-24 00:21:33 +0900

    fix: 모듈 import 에러 수정

--- src/components/ThreeBackground/ThreeBackground.tsx
+++ src/components/ThreeBackground/ThreeBackground.tsx
@@ -2,8 +2,8 @@
 import { colors } from '@/utils/colors';
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 
 /**
  * Three.js 3D 배경 컴포넌트
